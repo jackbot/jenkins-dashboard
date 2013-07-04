6d572de80a4d04de6cd90c97f527234c91f3275e
Jack Weeden <jack@ajack.org>
2013-07-04 15:49:22 +0100

    Some CSS changes. Added a spinner

--- app/public/javascripts/dashboard.js
+++ app/public/javascripts/dashboard.js
@@ -1,6 +1,8 @@
 var Dashboard = {
   request: null,
   template: null,
+  spinner: null,
+  refreshTimeout: 10000,
 
   init: function() {
     Dashboard.request = new XMLHttpRequest();
@@ -9,17 +11,25 @@ var Dashboard = {
     var source = document.getElementById("dashboard-template").innerHTML;
     Dashboard.template = Handlebars.compile(source);
 
+    Dashboard.spinner = document.getElementById("spinner");
+
     Dashboard.update();
-    setInterval(Dashboard.update, 5000);
+    setInterval(Dashboard.update, Dashboard.refreshTimeout);
   },
 
   renderBody: function(e) {
     var resp = JSON.parse(e.target.responseText);
     document.getElementById("rendered-body").innerHTML = Dashboard.template(resp);
+    Dashboard.toggleSpinner();
   },
 
   update: function() {
+    Dashboard.toggleSpinner();
     Dashboard.request.open('GET', "/ajax", true);
     Dashboard.request.send(null);
+  },
+
+  toggleSpinner: function() {
+    Dashboard.spinner.style.display = (Dashboard.spinner.style.display == "none") ? "block" : "none";
   }
 };
